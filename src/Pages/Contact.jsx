7a62061502164adeaea02aca8821eefa630074af
aboisam <samsonaboi10@gmail.com>
2025-05-18 21:04:56 +0100

    update on contact page

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from '../Component/header'
 
 const Contact = () => {
   return (
-    <div> Contact </div> 
+    <div>
+      <Header/>
+    </div>
   )
 }
 
